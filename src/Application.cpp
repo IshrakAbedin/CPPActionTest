9677afc90208b5187ab72d520195d82a366501a3
Ishrak Abedin <ishrak.abedin.a1@gmail.com>
2023-11-24 00:47:39 +0600

    Added config-wise printing

--- src/Application.cpp
+++ src/Application.cpp
@@ -6,6 +6,13 @@ int main()
 {
 	using namespace std::string_literals;
 
+#ifdef DEBUG
+	PrintStuffInALine("<<Running in Debug Mode>>");
+#endif // DEBUG
+#ifdef NDEBUG
+	PrintStuffInALine("<<Running in Release Mode>>");
+#endif // NDEBUG
+
 	PrintStuffInALine(100);
 	PrintStuffInALine(25.25f);
 	PrintStuffInALine("Hello");
